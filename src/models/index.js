4dd3acd2820347f9e7a689bd907ba3761532b9ce
anuragbhatt408 <anuragbhatt408@gmail.com>
2026-05-02 17:59:54 +0530

    updated index.js inside modal

--- src/models/index.js
+++ src/models/index.js
@@ -8,6 +8,16 @@ import UserPhoto from './UserPhoto.js';
 const connectDB = async () => {
   await sequelize.authenticate();
   console.log('Database connection established.');
+  const [[{ DATA_TYPE, COLUMN_TYPE }]] = await sequelize.query(
+    `SELECT DATA_TYPE, COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+     WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'users' AND COLUMN_NAME = 'id'`
+  );
+  if (DATA_TYPE === 'bigint' && !COLUMN_TYPE.includes('unsigned')) {
+    await sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
+    await sequelize.query('ALTER TABLE users MODIFY id BIGINT UNSIGNED AUTO_INCREMENT');
+    await sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
+    console.log('Migrated users.id to BIGINT UNSIGNED.');
+  }
   await sequelize.sync();
   console.log('Database synced.');
 };
